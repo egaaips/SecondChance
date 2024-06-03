bc958ff726e7e15b012d89099a7f1ebd034ba352
Ega Indah Permatasari <egaaips@gmail.com>
2024-06-03 13:55:33 +0700

    update js and modify about page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,23 +1,67 @@
 $(document).ready(function () {
   $(".hamburger").click(function () {
-    $(".top-menu").css("right", "0");
+      $(".top-menu").css("right", "0");
   });
   $(".close").click(function () {
-    $(".top-menu").css("right", "-100vw");
+      $(".top-menu").css("right", "-100vw");
   });
-
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
 });
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".item");
+  if (index >= slides.length) {
+      currentIndex = 0;
+  } else if (index < 0) {
+      currentIndex = slides.length - 1;
+  } else {
+      currentIndex = index;
+  }
+
+  const newTransform = -currentIndex * 100 + "%";
+  document.querySelector(
+      ".list"
+  ).style.transform = `translateX(${newTransform})`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentIndex);
+});
+
+/* ============= Vision & Mission Accordion ============= */
+const accordionGoalsItem = document.querySelectorAll(".goals_item");
+
+accordionGoalsItem.forEach((item) => {
+  const accordionHeader = item.querySelector(".goals_header");
+
+  accordionHeader.addEventListener("click", () => {
+      const openItem = document.querySelector(".accordion-open");
+
+      toggleItem(item);
+
+      if (openItem && openItem !== item) {
+          toggleItem(openItem);
+      }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".goals_content");
+
+  if (item.classList.contains("accordion-open")) {
+      accordionContent.removeAttribute("style");
+      item.classList.remove("accordion-open");
+  } else {
+      accordionContent.style.height = accordionContent.scrollHeight + "px";
+      item.classList.add("accordion-open");
+  }
+};
