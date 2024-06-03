123aece21488390ca8e9168dd40c2a3d121c3bb4
Ega Indah Permatasari <egaaips@gmail.com>
2024-06-03 13:27:03 +0700

    add donation page

--- resources/views/donate/donasi.blade.php
+++ resources/views/donate/donasi.blade.php
@@ -2,9 +2,45 @@
 
 @section('main')
     <section id="donate">
-        <h2>Donasi</h2>
-        <div class="donation-container">
-            <p>ini adalah halaman donasi</p>
+        <h2>Syarat dan Ketentuan Donasi</h2>
+        <div class="term-condition">
+            <div class="card-tnc">
+                    <img src="assets/img/donasi/12.png" alt="icon">
+                <div class="desc">
+                    <h4>Mengisi Form Donasi</h4>
+                    <p>Wajib mengisi form terlebih dahulu terutama jika metode pengiriman yang dipilih adalah menggunakan kurir jika tidak maka paket tidak akan diterima.</p>
+                </div>
+            </div>
+            <div class="card-tnc">
+                    <img src="assets/img/donasi/15.png" alt="icon">
+                <div class="desc">
+                    <h4>Kondisi Barang</h4>
+                    <p>Pastikan baju masih layak pakai, tidak kusam, berwarna kekuningan, kancing/ritsleting tidak copot, dan tidak sobek/bolong.</p>
+                </div>
+            </div>
+            <div class="card-tnc">
+                    <img src="assets/img/donasi/13.png" alt="icon">
+                <div class="desc">
+                    <h4>Bentuk Donasi</h4>
+                    <p>Donasi harus berupa pakaian atau aksesoris berbahan dasar kain (bukan pakaian dalam atau atribut perusahaan/lembaga/partai).</p>
+                </div>
+            </div>
+            <div class="card-tnc">
+                    <img src="assets/img/donasi/14.png" alt="icon">
+                <div class="desc">
+                    <h4>Kebersihan</h4>
+                    <p>Pastikan pakaian sudah dicuci terlebih dahulu dan dikirim dalam keadaan bersih sebelum Anda mendonasikannya kepada kami.</p>
+                </div>
+            </div>
+            
+        </div>
+
+        <div id="benefit" class="benefit">
+            <h2>Keuntungan Donasi</h2>
+        </div>
+
+        <div id="form-donation" class="form-donation">
+            <h2>Formulir Donasi</h2>
         </div>
     </section>
 @endsection
